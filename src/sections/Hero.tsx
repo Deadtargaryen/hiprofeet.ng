af97c935b0538b3006b9878f5dc6e2ea49ab1664
Deadtargaryen <mannyetukudoh@gmail.com>
2024-08-03 00:01:05 +0100

    Header complete, Hero 30%

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,20 @@
+export const Hero = () => {
+    return (
+        <section>
+            <div className="container">
+                <div>
+                    <div className="">Version 1.0 is here</div>
+                    <h1 className="">Strategic Social Media Solutions for your Brand</h1>
+                    <p>
+                    Drive Engagement, Boost Visibility, and Accelerate Growth with Tailored Strategies. 
+                    Track your progress & Celebrate your successes
+                    </p>
+                    <div>
+                        <button>Get Started</button>
+                        <button>Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
