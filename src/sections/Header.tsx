5b59aab4549b4b6b0480567e0fc4b497110eb7ea
Deadtargaryen <mannyetukudoh@gmail.com>
2024-08-02 21:33:09 +0100

    config config config

---
+++ src/sections/Header.tsx
@@ -0,0 +1,10 @@
+import ArrowRight from '@/assets/arrow-right.svg'
+
+export const Header = () => {
+    return (
+    <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+      <p>Get started for free</p>
+        <ArrowRight className="h-4 w-4 inline-flex justify-center"/>
+    </div>
+    );
+}
